ec3d5a08482f253fec30032705a2a833d55b4f66
c-dez <hdezcruzcarlos@gmail.com>
2024-01-22 20:33:18 -0600

    added in PlayerHealth healt values

--- player/PlayerHealth.cs
+++ player/PlayerHealth.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private float playerHealth = 100f;
+    [SerializeField] private float playerBaseHealth = 100f;
+    [SerializeField] private float playerMaxHealtMod = 1f;
+    private float playerMaxHealth;
+    private float playerCurrentHealth;
     void Start()
-
     {
-        
+        playerMaxHealth = playerBaseHealth * playerMaxHealtMod;
+        playerCurrentHealth = playerMaxHealth;
     }
 
     // Update is called once per frame
